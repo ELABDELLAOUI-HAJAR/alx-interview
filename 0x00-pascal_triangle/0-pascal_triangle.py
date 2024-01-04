14eab31f91941427b64389db04179adbb063f093
HAJAR EL ABDELLAOUI <125801785+ELABDELLAOUI-HAJAR@users.noreply.github.com>
2024-01-04 09:50:25 +0100

    Implement a function that return Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """Definition of pascal_triangle function
+      args: n the length od the triangle
+      Returns a list of lists of integers representing
+      the Pascal’s triangle
+    """
+
+    if n <= 0:
+        return []
+
+    pascal_triangle = [[1]]
+    while len(my_triangles) != n:
+        last_elmt = pascal_triangle[-1]
+        next_elmt = [1]
+        for i in range(0, len(last_elmt) - 1):
+            next_elmt.append(last_elmt[i] + last_elmt[i + 1])
+        next_elmt.append(1)
+        pascal_triangle.append(next_elmt)
+    return pascal_triangle
