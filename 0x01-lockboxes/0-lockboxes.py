49736d008efe533843e43648d2591b9938e89eee
HAJAR <elabdellaoui.hajar@gmail.com>
2024-01-09 19:19:11 +0100

    lockboxes problem solve

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+def canUnlockAll(boxes):
+    """ Method that return True if all boxes can be opened, 
+    else return False 
+    """
+
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # This Repository will contain all the interview preparation projects
+
+| project | Description |
+| ------- | ----------- |
+| 0x00. Pascal's Triangle | function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n |
+| 0x01. Lockboxes | Method that determines if all the boxes can be opened |
