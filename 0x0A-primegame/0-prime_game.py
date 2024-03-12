5b5d5abfe1ae33c1651bf9dc9e3a688504164e63
HAJAR <elabdellaoui.hajar@gmail.com>
2024-03-12 14:48:55 +0000

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for Prime game """
+
+
+def isWinner(x, nums):
+    """function to get the winner of the prime game """
+    if x < 1 or not nums:
+        return None
+
+    x_maria, x_ben = 0, 0
+
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        x_ben += primes_count % 2 == 0
+        x_maria += primes_count % 2 == 1
+    if x_maria == x_ben:
+        return None
+    return 'Maria' if x_maria > x_ben else 'Ben'
